385d69a49df319bc35a618614cd29177be4c977f
Sireken <43463273+Sireken@users.noreply.github.com>
2018-11-16 23:58:08 +0100

    Add files via upload

---
+++ 23.cpp
@@ -0,0 +1,17 @@
+ #include "std_lib_facilities.h"
+int main()
+try {
+          int i=0;int j=1; while (i>10) ++j; if (j>i) cout << "Success!\n";
+          keep_window_open(); 
+          return 0;
+}
+catch (exception& e) {
+          cerr << "error: " << e.what() << '\n';
+          keep_window_open();
+          return 1;
+}
+catch (...) {
+          cerr << "Oops: unknown exception!\n";
+          keep_window_open();
+          return 2;
+}
---
+++ 24.cpp
@@ -0,0 +1,17 @@
+ #include "std_lib_facilities.h"
+int main()
+try {
+         int x = 4; float d = 5/(2.125*x/5); if (d=2*x+0.5) cout << "Success!\n";
+          keep_window_open(); 
+          return 0;
+}
+catch (exception& e) {
+          cerr << "error: " << e.what() << '\n';
+          keep_window_open();
+          return 1;
+}
+catch (...) {
+          cerr << "Oops: unknown exception!\n";
+          keep_window_open();
+          return 2;
+}
